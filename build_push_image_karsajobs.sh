ed8c0bb752634ae35ae996fbde6b9ba56ab99aa8
nexterid <nexter.three@gmail.com>
2023-11-04 02:15:55 +0000

    membuat build image

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Membuat Docker image dari Dockerfile
+docker build -t karsajobs.
+
+# Melihat daftar image di lokal
+docker images
+
+# Mengubah nama image agar sesuai dengan format Docker Hub
+docker tag karsajobs nexterid/karsajobs:latest
+
+# login ke docker hub
+docker login
+
+# mengupload image ke docker hub
+docker push nexterid/karsajobs:latest
+
+# informasi ketika upload image ke docker hub selesai
+echo "Image berhasil diunggah ke Docker Hub (atau GitHub Packages)."
