47c69fd4262a6bd2da9719446ebf2cba52991a58
nexterid <nexter.three@gmail.com>
2023-11-04 04:58:42 +0000

    update build push images

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # Membuat Docker image dari Dockerfile
-docker build -t karsajobs .
+docker build -t karsajobs-ui .
 
-# Melihat daftar image di lokal
-docker images
+# Authenticate with GitHub Package Registry 
+echo $CR_PAT | docker login docker.pkg.github.com -u nexterid --password-stdin
 
-# Mengubah nama image agar sesuai dengan format Docker Hub
-docker tag karsajobs nexterid/karsajobs:latest
+# Mengubah nama image agar sesuai dengan format Docker Hub /github pakage
+docker tag karsajobs-ui docker.pkg.github.com/nexterid/a433-microservices/karsajobs-ui:latest
 
-# login ke docker hub
-docker login
+# mengupload image ke docker hub/github package
+docker push docker.pkg.github.com/nexterid/a433-microservices/karsajobs-ui:latest
 
-# mengupload image ke docker hub
-docker push docker.pkg.github.com/nexterid/a433-microservices/karsajobs:latest
-
-# informasi ketika upload image ke docker hub selesai
+# informasi ketika upload image ke docker hub/github package selesai
 echo "Image berhasil diunggah ke Docker Hub (atau GitHub Packages)."
