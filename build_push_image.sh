15537b9cc717620810155a969cd930ebce2ff286
nexterid <nexter.three@gmail.com>
2023-11-17 12:51:28 +0700

    add dockerfile and build push image

---
+++ build_push_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Membuat Docker image dari Dockerfile
+docker build -t order-service .
+
+# Authenticate with GitHub Package Registry 
+echo $CR_PAT | docker login docker.pkg.github.com -u nexterid --password-stdin
+
+# Mengubah nama image agar sesuai dengan format Docker Hub /github pakage
+docker tag order-service docker.pkg.github.com/nexterid/a433-microservices/order-service:latest
+
+# mengupload image ke docker hub/github package
+docker push docker.pkg.github.com/nexterid/a433-microservices/order-service:latest
+
+# informasi ketika upload image ke docker hub/github package selesai
+echo "Image berhasil diunggah ke Docker Hub (atau GitHub Packages)."
